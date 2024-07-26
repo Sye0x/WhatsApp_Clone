d1558d3a92c207948b987ec5cb8e3dafdf2a1361
unknown <s.r.mahamid@gmail.com>
2024-07-26 08:28:08 -0700

    ChatComponent

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,22 @@
 import { Text, View } from "react-native";
-import ChatItem from "@/components/chat";
+import ChatItem from "@/components/chatlist";
+
+const chat = {
+  id: "1",
+  user: {
+    name: "Sara",
+    image: "./assets/images/Flower.jpg",
+  },
+  lastmessage: {
+    message: "Hello",
+    time: "1:50 am",
+  },
+};
 
 export default function Index() {
   return (
     <View>
-      <ChatItem />
+      <ChatItem chatdata={chat} />
     </View>
   );
 }
---
+++ components/chatlist.tsx
@@ -0,0 +1,68 @@
+import { Text, View, Image, StyleSheet } from "react-native";
+
+const ChatItem = ({ chatdata }: any) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.datacontainer}>
+        <Image style={styles.image} source={{ uri: chatdata.user.image }} />
+        <View style={styles.textstyle}>
+          <View style={styles.textContainer}>
+            <Text style={styles.name} numberOfLines={1} ellipsizeMode="tail">
+              {chatdata.user.name}
+            </Text>
+            <Text style={styles.message} numberOfLines={1} ellipsizeMode="tail">
+              {chatdata.lastmessage.message}
+            </Text>
+          </View>
+          <Text style={styles.time}>{chatdata.lastmessage.time}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "95%",
+    height: 80,
+    margin: 5,
+  },
+  image: {
+    height: 65,
+    width: 65,
+    borderRadius: 35,
+  },
+  datacontainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  textstyle: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginLeft: 10,
+  },
+  textContainer: {
+    flex: 1,
+    marginRight: 10,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: "bold",
+    paddingBottom: 10,
+  },
+  message: {
+    fontSize: 16,
+    color: "gray",
+    paddingLeft: 5,
+  },
+  time: {
+    alignSelf: "flex-start",
+    marginTop: 5,
+    fontSize: 14,
+    color: "gray",
+  },
+});
+
+export default ChatItem;
